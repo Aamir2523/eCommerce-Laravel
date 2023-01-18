559c43b2112c14cb3316f59b1b895eabd7aa8083
Aamir <aamir.patel@kgn-software.com>
2023-01-18 19:24:16 +0530

    Product page done

--- database/seeders/ProductSeeder.php
+++ database/seeders/ProductSeeder.php
@@ -15,20 +15,31 @@ class ProductSeeder extends Seeder
     public function run()
     {
         DB::table('products')->insert([
-                [
-                    'name' => "Apple 14 pro",
-                    'price' => "1,00,000",
-                    'description' => "Apple 14 pro smartphone",
-                    "category" => "Mobile",
-                    "gallery" => "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg"
-                ],
-                [
-                    'name' => "Realme",
-                    'price' => "12,000",
-                    'description' => "Realme smartphone",
-                    "category" => "Mobile",
-                    "gallery" => "https://cdn1.smartprix.com/rx-iKWyXqzam-w1200-h1200/KWyXqzam.jpg"
-                ]
-            ]);
+            [
+                'name' => "Apple 14 pro",
+                'price' => "1,00,000",
+                'description' => "Apple 14 pro smartphone",
+                "category" => "Mobile",
+                "gallery" => "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg"
+            ],
+            [
+                'name' => "Realme",
+                'price' => "12,000",
+                'description' => "Realme smartphone",
+                "category" => "Mobile",
+                "gallery" => "https://cdn1.smartprix.com/rx-iKWyXqzam-w1200-h1200/KWyXqzam.jpg"
+            ]
+        ]);
+
+
+        // single product add and multiple product add(above code)
+
+        // DB::table('products')->insert([
+        //     'name' => "Apple 14 pro",
+        //     'price' => "1,00,000",
+        //     'description' => "Apple 14 pro smartphone",
+        //     "category" => "Mobile",
+        //     "gallery" => "https://m.media-amazon.com/images/I/71xb2xkN5qL._SL1500_.jpg"
+        // ]);
     }
 }
